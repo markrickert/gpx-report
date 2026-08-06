abe008d41b07dbc12515d4dd9c33351970eb10a1
root <root@gpx-report.fawn-porgy.ts.net>
2026-08-06 04:20:22 +0000

    Add km/miles unit toggle, default to imperial
    Distance/speed/elevation display was hardcoded metric. Added a
    UnitsProvider React context (frontend/src/units.jsx, localStorage-
    persisted) with a nav toggle, and wired Dashboard and ActivityDetail
    (including the elevation chart's axis values/labels/domain) to it.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,15 +4,18 @@ import { ApolloProvider } from "@apollo/client";
 import { BrowserRouter } from "react-router-dom";
 import "leaflet/dist/leaflet.css";
 import { apolloClient } from "./apolloClient.js";
+import { UnitsProvider } from "./units.jsx";
 import App from "./App.jsx";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ApolloProvider client={apolloClient}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <UnitsProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </UnitsProvider>
     </ApolloProvider>
   </React.StrictMode>
 );
